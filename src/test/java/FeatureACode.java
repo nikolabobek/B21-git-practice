9aefcd9520d92483a18719f6c3e2ad1b2d6e8bc7
nikolabobek <nikolabobek@gmail.com>
2021-01-26 14:23:06 -0500

    more work on featureA on new branch

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -3,6 +3,7 @@ public class FeatureACode {
     public static void main(String[] args) {
 
         System.out.println("Awesome work for A started");
+        System.out.println("B21 is learning more git with review");
     }
 
 }
